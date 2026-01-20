861b3e0cd98fa6697d1c600e75aa37947010a4a3
Tabitha Rowland <tabirowland@gmail.com>
2026-01-20 11:58:02 -0800

    Update main.cpp

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -1,5 +1,12 @@
 /*
-Skeleton code for storage management
+Assignment 2: Storage Manager
+Minh Huynh
+huynhmin
+huynhmin@oregonstate.edu
+
+Tabitha Rowland
+rowlanta
+rowlanta@oregonstate.edu
 */
 
 #include <string>
@@ -15,23 +22,28 @@ Skeleton code for storage management
 #include "classes.h"
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     // Initialize the Storage Manager Class with the Binary .dat file name we want to create
     StorageManager manager("EmployeeRelation.dat");
 
-    // Assuming the Employee.CSV file is in the same directory, 
+    // Assuming the Employee.CSV file is in the same directory,
     // we want to read from the Employee.csv and write into the new data_file
     manager.createFromFile("Employee.csv");
 
     // Searching for Employee IDs Using [manager.findAndPrintEmployee(id)]
-    /***TO_DO***/ 
-    
+    /***TO_DO***/
+
     // If an ID was provided on the command line, search it first
-    if (argc >= 2) {
-        try {
+    if (argc >= 2)
+    {
+        try
+        {
             int64_t id = stoll(argv[1]);
             manager.findAndPrintEmployee(id);
-        } catch (...) {
+        }
+        catch (...)
+        {
             cerr << "Invalid id argument: " << argv[1] << endl;
             return 1;
         }
@@ -39,12 +51,13 @@ int main(int argc, char* argv[]) {
 
     // Then allow multiple searches (one id at a time) until EOF (Ctrl-D)
     int64_t q;
-    while (true) {
+    while (true)
+    {
         cout << "Enter Employee id to search (Ctrl-D to quit): ";
-        if (!(cin >> q)) break;
+        if (!(cin >> q))
+            break;
         manager.findAndPrintEmployee(q);
     }
-
 
     return 0;
 }
