129e86e20da42f64868017558e06c1ae6af3cbdc
minhhuynh <71852804+minhhuynh103@users.noreply.github.com>
2026-02-16 01:39:24 -0800

    update main
    add main function to receive employee IDs

--- assignment 4/main.cpp
+++ assignment 4/main.cpp
@@ -7,14 +7,20 @@
 using namespace std;
 
 
-int main() {
+int main(int argc, char** argv) {
 
-    // Create the index
+    // Create the index from Employee.csv
     LinearHashIndex hashIndex("EmployeeIndex");
     hashIndex.createFromFile("Employee.csv");
 
 
     // TODO: You'll receive employee IDs as arguments, process them to retrieve the record, or display a message if not found. 
+        for (int k = 1; k < argc; k++) {
+            int id = stoi(string(argv[k]));
+            cout << "Searching for ID=" << id << "\n";
+            hashIndex.findAndPrintEmployee(id);
+            cout << "---\n";
+        }
 
     return 0;
 }
